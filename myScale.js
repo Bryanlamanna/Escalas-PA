7b6fdf05d5c06edc9e4bb014c92f132420989716
Bryan Lamanna <lamannabryan@gmail.com>
2024-02-11 21:38:38 -0300

    change font weighttt

--- myScale.js
+++ myScale.js
@@ -111,7 +111,7 @@ function createMyScale() {
     saveScale();
   
     // Atualizar o titulo da pagina com o primeiro dia do mes
-    document.querySelector('.title').innerHTML =('Escala atualizada para o mês de ' + mesSeguinte +'/'+ anoAtual + '.');
+    document.querySelector('.title').innerHTML =('Escala ATUAL do o mês de ' + mesSeguinte +'/'+ anoAtual + '.');
   
  }
  
