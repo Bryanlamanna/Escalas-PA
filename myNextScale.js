84b69b7cb457fcab7a88c4908c72c3a159b13693
Bryan Lamanna <lamannabryan@gmail.com>
2024-04-09 10:08:44 -0300

    update mynextScale.js

--- myNextScale.js
+++ myNextScale.js
@@ -53,6 +53,21 @@ const adrianoTD = document.querySelectorAll('.adriano td');
  const menuBtn = document.querySelector('.menuBtn');
  const menu = document.querySelector('.menu');
  const closeMenuBtn = document.querySelector('.closeMenu'); 
+ const fixosBtn = document.querySelector('.goToFixos');
+
+ fixosBtn.addEventListener('click', () => {
+  if (modalOn) {
+    modalChave.style.display = 'none'; 
+    modalOn = false;
+  } else {
+    menu.style.right = '-100%';
+    setTimeout(() => {
+      menu.style.display = 'none';
+    }, 50);
+    modalChave.style.display = 'flex';
+    modalOn = true;
+  }
+ })
 
 
  menuBtn.addEventListener('click', () => {
@@ -313,12 +328,15 @@ confirmBtn.addEventListener('click', () => {
       cell.style.backgroundColor = 'white';
       cell.style.color = 'black';
     })
+  } else if (chave === '4000') {
+    location.href = 'trocaFixos.html';
   } else {
     alert('Chave Incorreta!');
     document.querySelector('.chave').value = '';
   }
   
 })
+
 
 function includeScript() {
   var script = document.createElement('script');
